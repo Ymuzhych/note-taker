9b764b679aa76ec6f3a156b39fcff245267050ff
Ymuzhych <myuliia2018@gmail.com>
2022-06-07 09:51:32 -0500

    set up routes for api/html

--- routes/apiRouts/index.js
+++ routes/apiRouts/index.js
@@ -1,2 +1,6 @@
 const router = require('express').Router();
 const notesRoutes = require('./notesRoutes');
+
+router.use(notesRoutes);
+
+module.exports = router;
--- routes/apiRouts/notesRoutes.js
+++ routes/apiRouts/notesRoutes.js
@@ -1,1 +1,28 @@
 const router = require('express').Router();
+
+const { createNewNote, deleteNote } = require('../../lib/notes');
+let { notesArray } = require('../../db/notes');
+
+// notes are available at api/notes in JSON 
+router.get('/notes', (req, res) => {
+  let results = notesArray;
+  res.json(results);
+});
+//Post route
+router.post('/notes', (req, res) => {
+
+  if(notesArray){
+  req.body.id = notesArray.length.toString();
+  } else 
+  {req.body.id = 0}
+  res.json(createNewNote(req.body, notesArray));
+});
+
+// Delete
+router.delete('/notes/:id', async (req, res) => {
+  const { id } = req.params
+  notesArray = await deleteNote(id, notesArray);
+  res.json(notesArray);
+});
+
+module.exports = router;
