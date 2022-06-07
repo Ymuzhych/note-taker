56a24657a1fe83ccf956b41e2967bc04206f019c
Ymuzhych <myuliia2018@gmail.com>
2022-06-07 10:42:22 -0500

    fix bug

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 const { createNewNote, deleteNote } = require('../../lib/notes');
-let { notesArray } = require('../../db/notes');
+let { notesArray } = require('../../db/db');
 
 // notes are available at api/notes in JSON 
 router.get('/notes', (req, res) => {
